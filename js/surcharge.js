ffa96dfb25b99afb8834e801ea2be73a6592a6ce
guillaumeperotunflux <guillaume.perot@unflux.fr>
2020-06-05 16:42:07 +0200

    Update surcharge.js

--- js/surcharge.js
+++ js/surcharge.js
@@ -1,4 +1,23 @@
-function showMoreAreas(e) {
+function showMoreAreas() {
   $('#business-areas .item-list').removeClass('visually-hidden');
   $('#showMoreAreas').hide()
 }
+
+$(function () {
+  $('.third-container').before("<div id='btn-scroll-top'></div>");
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#btn-scroll-top').fadeIn();
+    } else {
+      $('#btn-scroll-top').fadeOut();
+    }
+  });
+
+  $('#btn-scroll-top').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+});
